aa8696b3f38775f835c135393a5a5e68237412bc
manucastelnovo <marnutskate@gmail.com>
2025-03-23 20:13:45 -0300

    add header and tablet componets to app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+   <HeaderComponent />
+   <TableComponent :photos="photosToShow" />
   </div>
 </template>
+
+
+<script setup>
+import { computed, ref, watch } from "vue";
+import { usePhotoStore } from "~/store/store.ts";
+
+
+const photo = usePhotoStore();
+const isInitialLoad = ref(true);
+
+
+const photosToShow = computed(() => {
+  if (isInitialLoad.value) {
+    return [];
+  }
+  return photo.displayedPhotos;
+});
+
+watch(
+  () => photo.displayedPhotos,
+  (newPhotos) => {
+    if (isInitialLoad.value && newPhotos.length > 0) {
+      isInitialLoad.value = false;
+    }
+  }
+);
+
+</script>
+
