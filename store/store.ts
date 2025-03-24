3260ba9df7a53a363ee297109509322018b128b1
manucastelnovo <marnutskate@gmail.com>
2025-03-24 13:56:54 -0300

    add store

--- store/store.ts
+++ store/store.ts
@@ -2,40 +2,38 @@ import { defineStore } from 'pinia';
 import axios from 'axios';
 import type { photo } from '~/models/photo';
 
-const API_URL = "https://jsonplaceholder.typicode.com/photos"; // Tu API
+const API_URL = "https://jsonplaceholder.typicode.com/photos"; 
 
 export const usePhotoStore = defineStore('photos', {
   state: () => ({
-    photos: [] as photo[], // Todas las fotos
-    displayedPhotos: [] as photo[], // Fotos que se muestran actualmente
-    currentIndex: 0, // Índice actual de donde empezar a mostrar las fotos
-    isLoading: false, // Indicador de carga
+    photos: [] as photo[], 
+    displayedPhotos: [] as photo[], 
+    currentIndex: 0, 
+    isLoading: false, 
   }),
 
   actions: {
-    async searchPhotos(albumQuery: []) {
-      if (this.isLoading) return; // Si ya estamos cargando, no hacemos otra petición
-
+    async searchPhotos(albumQueryUrl: string) {
+      if (this.isLoading) return; 
       this.isLoading = true;
-      const albumQueryUrl = albumQuery
-        .map((album) => `&albumId=${album}`)
-        .join("");
-
+      this.displayedPhotos = []
+      this.currentIndex = 0
       try {
-        const response = await axios.get(`${API_URL}?${albumQueryUrl}`); // Consulta para todas las fotos
-        this.photos = response.data; // Guardamos todas las fotos
-        this.loadMorePhotos(); // Cargamos las primeras fotos para mostrar
+        const response = await axios.get(`${API_URL}?${albumQueryUrl}`); 
+        this.photos = response.data; 
+        this.loadMorePhotos();
+
       } catch (error) {
         console.error("Error fetching photos", error);
       } finally {
-        this.isLoading = false; // Terminamos de cargar
+        this.isLoading = false; 
       }
     },
 
     loadMorePhotos() {
-      const photosToShow = this.photos.slice(this.currentIndex, this.currentIndex + 20); // Seleccionamos un bloque de fotos para mostrar
-      this.displayedPhotos = [...this.displayedPhotos, ...photosToShow]; // Añadimos esas fotos a las que ya se están mostrando
-      this.currentIndex += 20; // Actualizamos el índice
+      const photosToShow = this.photos.slice(this.currentIndex, this.currentIndex + 20); 
+      this.displayedPhotos = [...this.displayedPhotos, ...photosToShow]; 
+      this.currentIndex += 30; 
     },
   },
 });
