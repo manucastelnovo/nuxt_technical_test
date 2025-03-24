eb2490d29f5cff1642602f382dce994164ec9726
manucastelnovo <marnutskate@gmail.com>
2025-03-24 13:55:34 -0300

    add table component

--- components/TableComponent.vue
+++ components/TableComponent.vue
@@ -7,17 +7,24 @@
         <div
           class="inline-block max-w-[600px] h-[600px] overflow-auto py-2 align-middle sm:px-6 lg:px-8"
         >
-          <table class="divide-y table-fixed w-[1251px] divide-gray-300">
-            <thead class="bg-white">
+          <div
+            v-if="isLoading"
+            class="w-full h-full flex justify-center items-center"
+          >
+            <CircularProgress />
+          </div>
+          <table v-else class="divide-y table-fixed w-[1251px] divide-gray-300">
+            <thead class="sticky -top-2 z-10">
               <tr>
                 <th
                   scope="col"
-                  class="sticky top-0 z-10 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0 w-16"
+                  class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-0 w-16"
                 >
                   <a href="#" class="group inline-flex">
                     Id
                     <span
-                      class="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
+                      class="ml-2 flex-none rounded group-hover:bg-gray-500"
+                      @click="sort('id', photos)"
                     >
                       <ChevronDownIcon class="size-5" aria-hidden="true" />
                     </span>
@@ -25,74 +32,34 @@
                 </th>
                 <th
                   scope="col"
-                  class="sticky top-0 z-10 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 w-26"
+                  class="px-3 py-3.5 text-left text-sm font-semibold w-26"
                 >
-                  <a href="#" class="group inline-flex">
-                    AlbumId
-                    <span
-                      class="ml-2 flex-none rounded bg-gray-100 text-gray-900 group-hover:bg-gray-200"
-                    >
-                      <ChevronDownIcon class="size-5" aria-hidden="true" />
-                    </span>
-                  </a>
+                  <a href="#" class="group inline-flex"> AlbumId </a>
                 </th>
                 <th
                   scope="col"
-                  class="sticky top-0 z-10 px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  class="px-3 py-3.5 text-left text-sm font-semibold"
                 >
-                  <a href="#" class="group inline-flex">
-                    Title
-                    <span
-                      class="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
-                    >
-                      <ChevronDownIcon
-                        class="invisible ml-2 size-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </a>
+                  <a href="#" class="group inline-flex"> Title </a>
                 </th>
                 <th
                   scope="col"
-                  class="sticky top-0 z-10 px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  class="px-3 py-3.5 text-left text-sm font-semibold"
                 >
-                  <a href="#" class="group inline-flex">
-                    Url
-                    <span
-                      class="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
-                    >
-                      <ChevronDownIcon
-                        class="invisible ml-2 size-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </a>
+                  <a href="#" class="group inline-flex"> Url </a>
                 </th>
                 <th
                   scope="col"
-                  class="sticky top-0 z-10 px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  class="px-3 py-3.5 text-left text-sm font-semibold"
                 >
-                  <a href="#" class="group inline-flex">
-                    Thumbnail Url
-                    <span
-                      class="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
-                    >
-                      <ChevronDownIcon
-                        class="invisible ml-2 size-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </a>
+                  <a href="#" class="group inline-flex"> Thumbnail Url </a>
                 </th>
               </tr>
             </thead>
-            <tbody
-              class="divide-y divide-gray-200 bg-white"
-              @scroll="handleScroll"
-            >
+            <tbody class="divide-y divide-gray-200" @scroll="handleScroll">
               <tr v-for="photo in photos" :key="photo.id">
                 <td
-                  class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0 truncate"
+                  class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-500 sm:pl-0 truncate"
                 >
                   {{ photo.id }}
                 </td>
@@ -120,9 +87,12 @@
                   {{ photo.thumbnailUrl }}
                 </td>
               </tr>
-              <tr ref="target" class="h-10 w-full" />
-              <tr  class="text-center p-4 w-full flex justify-center">
-
+              <tr ref="target" class="h-10 w-full dis" />
+              <tr
+                v-if="photoStore.isLoading"
+                class="w-[600px] flex justify-center mt-4"
+              >
+                <CircularProgress />
               </tr>
             </tbody>
           </table>
@@ -137,23 +107,41 @@ import { ChevronDownIcon } from "@heroicons/vue/20/solid";
 import { useIntersectionObserver } from "@vueuse/core";
 import { usePhotoStore } from "~/store/store.ts";
 import { useTemplateRef } from "vue";
+import CircularProgress from "./CircularProgress.vue";
+import { stringToAlbumList } from "~/helpers/albumsId.ts";
 
-// Referencia al div del "infinite scroll" (último elemento de la lista)
 const target = useTemplateRef("target");
 
-// Usamos el store de fotos
 const photoStore = usePhotoStore();
+const isLoading = ref(true);
+const searchQuery = ref("");
+const sortOrder = ref("asc");
 
-// Detectamos la intersección del último elemento de la lista
 useIntersectionObserver(target, ([entry]) => {
   if (entry?.isIntersecting && !photoStore.isLoading) {
+    isLoading.value = true;
     photoStore.loadMorePhotos();
-    console.log("entre");
+    isLoading.value = false;
   }
 });
 
-// Cargamos las primeras fotos cuando el componente se monta
-// photoStore.loadPhotos([1, 2, 3])
+onMounted(async () => {
+  searchQuery.value = localStorage.getItem("albumIds");
+  const ids = stringToAlbumList(searchQuery.value);
+  await photoStore.searchPhotos(ids);
+  isLoading.value = false;
+});
+
+const sort = (key, photosToSort) => {
+  sortOrder.value = sortOrder.value === "asc" ? "desc" : "asc";
+  photosToSort.sort((a, b) => {
+    if (sortOrder.value === "asc") {
+      return a[key] - b[key];
+    } else {
+      return b[key] - a[key];
+    }
+  });
+};
 
 defineProps({
   photos: {
